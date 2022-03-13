c18890918ee2d7006658ae355818b35d1c7e5630
LeoAndo <leo.777.info@gmail.com>
2022-03-13 16:11:42 +0900

    compose sample

--- LottieComposeSample/app/src/main/java/com/example/lottiecomposesample/MainActivity.kt
+++ LottieComposeSample/app/src/main/java/com/example/lottiecomposesample/MainActivity.kt
@@ -3,41 +3,93 @@ package com.example.lottiecomposesample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.*
 import com.example.lottiecomposesample.ui.theme.LottieComposeSampleTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             LottieComposeSampleTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                Scaffold(
+                    topBar = {
+                        SmallTopAppBar(
+                            title = {
+                                Text(text = "LottieComposeSample")
+                            },
+                        )
+                    },
+                    content = { Greeting() }
+                )
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+private fun Greeting() {
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.animation_party_parrot))
+    var isPlaying by remember { mutableStateOf(false) }
+    var restartOnPlay by remember { mutableStateOf(false) }
+    val progress by animateLottieCompositionAsState(
+        composition = composition,
+        speed = 0.2f,
+        isPlaying = isPlaying,
+        restartOnPlay = restartOnPlay,
+        cancellationBehavior = LottieCancellationBehavior.Immediately,
+        iterations = LottieConstants.IterateForever,
+        clipSpec = LottieClipSpec.Progress(0f, 1f),
+    )
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    LottieComposeSampleTheme {
-        Greeting("Android")
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(20.dp)
+    ) {
+        LottieAnimation(
+            composition,
+            progress,
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+        ) {
+            OutlinedButton(onClick = {
+                isPlaying = true
+                restartOnPlay = false
+            }, modifier = Modifier.weight(1f)) {
+                Text(text = "resume")
+            }
+            OutlinedButton(onClick = {
+                isPlaying = false
+                restartOnPlay = false
+            }, modifier = Modifier.weight(1f)) {
+                Text(text = "pause")
+            }
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+        ) {
+            OutlinedButton(onClick = {
+                isPlaying = true
+                restartOnPlay = true
+            }, modifier = Modifier.weight(1f)) {
+                Text(text = "play")
+            }
+            OutlinedButton(onClick = {
+                isPlaying = false
+                restartOnPlay = true
+            }, modifier = Modifier.weight(1f)) {
+                Text(text = "cancel")
+            }
+        }
     }
 }
